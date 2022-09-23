91aebdcb4362f412e9fb12048b37207554b9799a
mac-mac-mac <macfbarron@gmail.com>
2022-09-23 19:53:58 +0000

    added form

--- todo.py
+++ todo.py
@@ -1,6 +1,6 @@
 #app.py
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
@@ -9,9 +9,11 @@ app = Flask(__name__)
 def hello():
     return render_template('home.html')
 
-@app.route("/addTodo", method=['POST'])
+@app.route("/addTodo", methods=['POST'])
 def addTodo():
-    #access body information
+    text1 = request.form['text1']
+    print(text1)
+    return(text1)
 
 
 if __name__ == "__main__":
@@ -21,8 +23,8 @@ if __name__ == "__main__":
 
 
 #All 4 of them pass information through query parameters and path parameters
-# query params. www.google.com?page=3&row=53782&user=joe
-# path parameters www.google.com/someRootURL/3/53782/joe
+#query parameters -> www.google.com?page=3&row=53782&user=joe
+#path parameters -> www.google.com/someRootURL/3/53782/joe
 
 #POST and PUT
 #Info will be passed in the body -> www.google.com
